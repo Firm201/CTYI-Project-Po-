6c96f643f035129b4a504cc3a81c16d98a025402
Firm201 <52652881+Firm201@users.noreply.github.com>
2019-07-08 20:03:10 +0100

    Add files via upload

--- Hangman.py
+++ Hangman.py
@@ -1,18 +1,20 @@
 import time
+import os
 name = raw_input("What is your name? ")
-print ("Hello, " + name, "Time to play hangman!")
-print ""
+print ("Hello, " + name, "Welcome to Hangman!")
+word = raw_input("Enter your word(Lowercase Please) ")
+os.system('clear')
 time.sleep(1)
 print ("Start guessing...")
 time.sleep(0.5)
-word = ("cbt")
+word = (word)
 guesses = ''
 turns = 10
 while turns > 0:         
     failed = 0               
     for char in word:      
-        if char in guesses:    
-            print char,    
+        if char in guesses:
+             print char,    
         else:
             print "_",     
             failed += 1    
@@ -27,4 +29,4 @@ while turns > 0:
         print "Wrong"    
         print "You have", + turns, 'more guesses' 
         if turns == 0:          
-            print "You Loose"
+            print ("You Loose, Your word was: " + word,)
